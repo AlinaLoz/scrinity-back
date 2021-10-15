b34adfadf6a66aae8b76217c17bc7d76d37e4622
Alina Lozyuk <32218750+AlinaLoz@users.noreply.github.com>
2021-10-16 00:08:40 +0300

    [HOTFIX] deploy 5 task (#7)
    * update deps
    
    * [HOTFIX] deploty 5 task
    
    * [HOTFIX] deploty 5 task

--- scripts/docker/api-build-and-publish.sh
+++ scripts/docker/api-build-and-publish.sh
@@ -14,7 +14,7 @@ docker logout
 # Login in AWS container registry with your AWS credentials
 aws ecr get-login-password --profile $PROFILE --region $REGION | docker login --username AWS --password-stdin $AWS_DOCKER_REGISTRY_URL
 # Build Docker image
-docker build -f "$(dirname "$0")/../../Dockerfile" -t $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" -t $DOCKER_APP_REPOSITORY:$DOCKER_LATEST_TAG "$(dirname "$0")/../../"
+docker build -f "$(dirname "$0")/../../docker/api/Dockerfile" -t $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" -t $DOCKER_APP_REPOSITORY:$DOCKER_LATEST_TAG "$(dirname "$0")/../../"
 
 # Tag Docker image
 docker tag $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" $AWS_DOCKER_REGISTRY_URL/$DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG"
