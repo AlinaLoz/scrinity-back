76cda575b53b513513aae6845c5ebe7d06387309
a.lozyuk <a.lozyuk@pixelplex.io>
2021-10-16 23:45:55 +0300

    [back-6] verify confirm code

--- libs/auth/src/strategies/jwt.strategy.ts
+++ libs/auth/src/strategies/jwt.strategy.ts
@@ -2,7 +2,6 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import * as CONFIG from 'config';
-// import { jwtConstants } from './constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
