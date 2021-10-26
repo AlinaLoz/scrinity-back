dce58d9f74f8d0c4ce62d0c3c737a947eea4e426
a.lozyuk <a.lozyuk@pixelplex.io>
2021-10-27 00:06:17 +0300

    [task-5] enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 import CONFIG from 'config';
+import helmet from 'helmet';
+import cors from 'cors';
 
 import { AppLogger } from '@libs/logger';
 import { ApiModule } from './api.module';
@@ -12,6 +14,17 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiModule, { logger });
   app.setGlobalPrefix('/api/v1');
   app.enableCors();
+  app.use(helmet());
+  const corsOptions = {
+    origin: (origin: string, callback: (data: null, value: boolean) => void): void => {
+      callback(null, true);
+    },
+    credentials: true,
+    methods: ['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
+    headers: ['x-user', 'X-Signature', 'accept', 'content-type', 'authorization'],
+  };
+  app.use(cors(corsOptions));
+
   setupSwagger(app);
   await app.listen(CONFIG.API_PORT, () => {
     logger.log(`api port: ${CONFIG.API_PORT}`);
