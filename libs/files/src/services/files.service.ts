92063d3016bb452c2de4cc40d03c3f3b164f505d
a.lozyuk <a.lozyuk@pixelplex.io>
2021-10-31 23:34:36 +0300

    [HOTFIX] TMulterFile

--- libs/files/src/services/files.service.ts
+++ libs/files/src/services/files.service.ts
@@ -1,12 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import { S3Service } from './s3.service';
+import { TMulterFile } from '@libs/files/types/files.types';
 
 @Injectable()
 export class FilesService {
   @Inject() private readonly s3Service: S3Service;
 
-  async uploadImages(files: Express.Multer.File[]): Promise<string[]> {
+  async uploadImages(files: TMulterFile[]): Promise<string[]> {
     return this.s3Service.uploadFiles(files);
   }
 }
