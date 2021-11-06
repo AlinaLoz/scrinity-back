15046857c4bd8261a812e767d8384919d03c68fd
a.lozyuk <a.lozyuk@pixelplex.io>
2021-11-06 20:47:11 +0300

    [back-8] update imageFileFilter

--- libs/files/src/utils/file.utils.ts
+++ libs/files/src/utils/file.utils.ts
@@ -2,6 +2,10 @@ import { UnprocessableEntityError } from '@libs/exceptions';
 import { ERRORS } from '@libs/constants';
 
 export const imageFileFilter = (req: Request, file: any, callback: (error: Error | null, acceptFile: boolean) => void) => {
+  if (file.mimetype.toLowerCase().includes('image/image')) {
+    file.originalname = Date.now() + '.' +file.originalname.toLowerCase();
+    file.mimetype = file.mimetype.replace('image/image', 'image');
+  } else
   if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
     return callback(
       new UnprocessableEntityError([{ field: '', message: ERRORS.INVALID_IMAGES_FORMAT }]),
