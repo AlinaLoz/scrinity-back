7907c96dc509ce99386d24e9768ae0c88d78eea7
a.lozyuk <a.lozyuk@pixelplex.io>
2022-04-02 10:42:24 +0300

    db autosync

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,42 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { INestApplication } from '@nestjs/common';
-import helmet from 'helmet';
-import cors, { CorsOptions } from 'cors';
-import cookieParser from 'cookie-parser';
 import CONFIG from 'config';
-
-import { AppLogger } from '@libs/logger';
-import packageJson from '../../../package.json';
+import { bootstrap } from '../../bootstrap';
 import { ApiModule } from './api.module';
 
-async function bootstrap(): Promise<void> {
-  const logger = new AppLogger('api.service');
-  const app = await NestFactory.create(ApiModule, { logger });
-  app.setGlobalPrefix('/api/v1');
-  app.use(helmet());
-  const corsOptions: CorsOptions = {
-    origin: (origin, callback): void => {
-      callback(null, true);
-    },
-    credentials: true,
-    methods: ['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
-    allowedHeaders: ['x-user', 'X-Signature', 'accept', 'content-type', 'authorization'],
-  };
-  app.use(cookieParser());
-  app.use(cors(corsOptions));
-  setupSwagger(app);
-  await app.listen(CONFIG.API_PORT, () => {
-    logger.log(`api port: ${CONFIG.API_PORT}`);
-  });
-}
-bootstrap();
-
-function setupSwagger(app: INestApplication): void {
-  if (!CONFIG.IS_OPEN_SWAGGER) {
-    return;
-  }
-  const config = new DocumentBuilder().setTitle('Documentation Api').setVersion(packageJson.version).build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-}
+bootstrap('api.service', ApiModule, CONFIG.API_PORT);
--- apps/manager/src/main.ts
+++ apps/manager/src/main.ts
@@ -1,42 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { INestApplication } from '@nestjs/common';
-import helmet from 'helmet';
-import cors, { CorsOptions } from 'cors';
-import cookieParser from 'cookie-parser';
 import CONFIG from 'config';
-
-import { AppLogger } from '@libs/logger';
-import packageJson from '../../../package.json';
+import { bootstrap } from '../../bootstrap';
 import { ManagerModule } from './manager.module';
 
-async function bootstrap(): Promise<void> {
-  const logger = new AppLogger('manager.service');
-  const app = await NestFactory.create(ManagerModule, { logger });
-  app.setGlobalPrefix('/api/v1');
-  app.use(helmet());
-  const corsOptions: CorsOptions = {
-    origin: (origin, callback): void => {
-      callback(null, true);
-    },
-    credentials: true,
-    methods: ['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
-    allowedHeaders: ['x-user', 'X-Signature', 'accept', 'content-type', 'authorization'],
-  };
-  app.use(cookieParser());
-  app.use(cors(corsOptions));
-  setupSwagger(app);
-  await app.listen(CONFIG.MANAGER_PORT, () => {
-    logger.log(`api port: ${CONFIG.MANAGER_PORT}`);
-  });
-}
-bootstrap();
-
-function setupSwagger(app: INestApplication): void {
-  if (!CONFIG.IS_OPEN_SWAGGER) {
-    return;
-  }
-  const config = new DocumentBuilder().setTitle('Documentation Manager').setVersion(packageJson.version).build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-}
+bootstrap('manager.service', ManagerModule, CONFIG.MANAGER_PORT);
